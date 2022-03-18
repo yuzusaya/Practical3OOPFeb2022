32f41424eb9704aec152292ab699dd04a6082eb5
沙耶 <yhsia@codersai.com>
2022-03-18 08:50:04 +0800

    Add project files.

---
+++ Practical3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Practical3;
+
+/*
+var studentId1 = "bcsxxxxx";
+var studentName = "xxxx";
+var studentId2 = "bcs123123"; 
+var studentName2 = "xxxx";
+Student student1 = new Student();
+
+Shape shape1 = new Shape();
+shape1.GetArea(2,2,2);
+*/
+Student student1 = new Student();
+student1.PrintName();
+Student student2 = new Student();
+student2.StudentId = 2;
+student2.PrintName();
+var firstName = student2.FirstName;
---
+++ Practical3/Shape.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3
+{
+    public class Shape
+    {
+        private int _privateField;
+        protected int _protectedField;
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        //overload function
+        public void GetArea(int width, int height)
+        {
+            Console.WriteLine(width*height);
+        }
+
+        public void GetArea(string width, int height)
+        {
+
+        }
+        public void GetArea(int width, string height)
+        {
+
+        }
+
+        public void GetArea(int width, int height, int length)
+        {
+
+        }
+    }
+
+    public class Triangle : Shape
+    {
+
+        //constructor - execute when an object is created
+        public Triangle()
+        {
+            
+        }
+
+        public void Method1()
+        {
+            Width = 1;
+            Height = 2;
+            _protectedField = 2;
+        }
+    }
+
+    public class Student
+    {
+        //field store data
+        private string _studentName;
+        private int _studentId=1;
+
+        //property will expose the field to public
+        public string StudentName
+        {
+            get
+            {
+                return "BCS"+_studentId;
+            }
+            set
+            {
+                _studentName=value;
+            }
+        }
+
+        //public string StudentName { get; set; }
+        //public string StudentId { get; set; }
+        public string FirstName { get; }
+        public string LastName { get;set; }
+
+        public int StudentId
+        {
+            get
+            {
+                return _studentId;
+            }
+            set
+            {
+                _studentId = value;
+            }
+        }
+
+        public string PrintName()
+        {
+            Console.WriteLine(StudentName);
+            return StudentName;
+        }
+    }
+
+    
+}
